0d0893bacada45e78a40eaf85a2eb7a0b39ab1a0
sajusun <sajuislam266@gmail.com>
2023-12-11 14:04:17 +0600

    bmi clculator app init

---
+++ lib/container_box.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContainerBox extends StatelessWidget {
+  ContainerBox({required this.boxColor, required this.childWidget});
+
+  final Color boxColor;
+  final Widget childWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: childWidget,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: boxColor,
+        
+      ),
+    );
+  }
+}
